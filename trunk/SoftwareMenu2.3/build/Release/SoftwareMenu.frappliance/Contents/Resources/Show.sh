512c9ff84cf49a5843f3d1ecdb1ab00f8eb62865
tomcool420 <tomcool420@5333c37c-0b2b-11de-b8f3-b9fb9eb71b04>
2009-03-08 14:25:29 +0000

    delete build

--- trunk/SoftwareMenu2.3/build/Release/SoftwareMenu.frappliance/Contents/Resources/Show.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /System/Library/CoreServices/Finder.app/Contents/Plugins\ \(disabled\)
-#cd ~/Documents/Backups/
-echo "frontrow" | sudo -S mount -uw /
-sudo mv $1.bak /System/Library/CoreServices/Finder.app/Contents/Plugins/$1.frappliance
--- trunk/SoftwareMenu2.3/build/Release/SoftwareMenu.frappliance/Contents/Resources/reset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "frontrow" | sudo -S command
-
-sudo kill `ps awwx | grep [F]inder | awk '{print $1}'`
-
-sudo open /System/Library/CoreServices/Finder.app
