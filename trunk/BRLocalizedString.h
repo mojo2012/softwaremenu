0f808b66db2d846ffd30c9b1050a4faee2d255e0
tomcool420 <tomcool420@5333c37c-0b2b-11de-b8f3-b9fb9eb71b04>
2009-03-08 13:55:08 +0000

    Deleting

--- trunk/BRLocalizedString.h
+++
@@ -1,8 +0,0 @@
-//#import <BackRow/BRLocalizedStringManager.h>
-
-// plugin-based NSLocalizedString macros
-// use genstrings -s BRLocalizedString -o <Language>.lproj to generate Localized.strings
-/*#define BRLocalizedString(key, comment) \
-    [BRLocalizedStringManager appliance:self localizedStringForKey:(key) inFile:nil]
-#define BRLocalizedStringFromTable(key, tbl, comment) \
-    [BRLocalizedStringManager appliance:self localizedStringForKey:(key) inFile:(tbl)]*/
