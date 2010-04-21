e72b6016fc0c1c034d8d8a89b8290a01e8927471
tomcool420 <tomcool420@5333c37c-0b2b-11de-b8f3-b9fb9eb71b04>
2010-04-21 20:06:44 +0000

    Removing Build

--- trunk/SoftwareMenu/build/Debug/SoftwareMenu.frappliance/Contents/Resources/hello.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "frontrow" | sudo -S command
-sudo rm -r /System/Library/CoreServices/Finder.app/Contents/Plugins/scripts.frappliance
-sudo mv ~/scripts.frappliance /System/Library/CoreServices/Finder.app/Contents/Plugins/scripts.frappliance 
-echo "installed please either Killall Finder or sudo Shutdown -r now"
