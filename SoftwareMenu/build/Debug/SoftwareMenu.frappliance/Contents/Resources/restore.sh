00adde9fb9c12b7d3f84c0ace8fe10dcc56643b5
tomcool420 <tomcool420@5333c37c-0b2b-11de-b8f3-b9fb9eb71b04>
2010-04-21 20:06:44 +0000

    Removing Build

--- SoftwareMenu/build/Debug/SoftwareMenu.frappliance/Contents/Resources/restore.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "frontrow" | sudo -S mount -uw /
-sudo cp -r /Users/frontrow/Documents/Backups/$1.bak /System/Library/CoreServices/Finder.app/Contents/Plugins/$1.frappliance
-cd ~/Documents/
-echo "restore" > done.txt
