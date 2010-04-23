b253c72deb54bd74ac816e5a6f6c023b64c26468
tomcool420 <tomcool420@5333c37c-0b2b-11de-b8f3-b9fb9eb71b04>
2010-04-23 18:13:50 +0000

    Script Options in progress

--- SoftwareMenu/Classes/SimpleMenus/SMScriptOptions.m
+++ SoftwareMenu/Classes/SimpleMenus/SMScriptOptions.m
@@ -7,6 +7,10 @@
 //
 
 #import "SMScriptOptions.h"
+#define ROOT_KEY        @"root"
+#define WAIT_KEY        @"wait"
+#define CUST_KEY        @"custom"
+#define MAIN_KEY        @"mainmenu"
 
 
 @implementation SMScriptOptions
@@ -26,11 +30,53 @@
 {
     self=[super init];
     [self setScriptName:scriptName];
-    _scriptOptions=[SMNewScriptsMenu scriptsOptions];
+    _scriptOptions=[[[SMNewScriptsMenu scriptsOptions] objectForKey:scriptName] mutableCopy];
+    if (_scriptOptions==nil) {
+        _scriptOptions=[[SMNewScriptsMenu defaultScriptOptions] mutableCopy];
+    }
+    [_scriptOptions retain];
     [self setListTitle:scriptName];
     id item=[BRTextMenuItemLayer menuItem];
-    [item setTitle:BRLocalizedString(@"Run As Root:",@"Run As Root")];
+    [item setTitle:BRLocalizedString(@"Use Custom Options:",@"Use Custom Options:")];
+    [_items addObject:item];
+    
+    item=[BRTextMenuItemLayer menuItem];
+    [item setTitle:BRLocalizedString(@"Run As Root:",@"Run As Root:")];
+    [_items addObject:item];
+    
+    item=[BRTextMenuItemLayer menuItem];
+    [item setTitle:BRLocalizedString(@"Wait For Completion:",@"Wait For Completion:")];
+    [_items addObject:item];
+    
+
+    
+    item=[BRTextMenuItemLayer menuItem];
+    [item setTitle:BRLocalizedString(@"Show on Main Menu:",@"Show on Main Menu")];
     [_items addObject:item];
     return self;
+}
+-(id)itemForRow:(long)row
+{
+    if (row>=[_items count]) {
+        return nil;
+    }
+    BRTextMenuItemLayer *item = [_items objectAtIndex:row];
+    switch (row) {
+        case 0:
+            [item setRightJustifiedText:([[_scriptOptions objectForKey:CUST_KEY] boolValue]?@"YES":@"NO")];
+            break;
+        case 1:
+            [item setRightJustifiedText:([[_scriptOptions objectForKey:ROOT_KEY] boolValue]?@"YES":@"NO")];
+            break;
+        case 2:
+            [item setRightJustifiedText:([[_scriptOptions objectForKey:WAIT_KEY] boolValue]?@"YES":@"NO")];
+            break;
+        case 3:
+            [item setRightJustifiedText:([[_scriptOptions objectForKey:MAIN_KEY] boolValue]?@"YES":@"NO")];
+            break;
+        default:
+            break;
+    }
+    return item;
 }
 @end
