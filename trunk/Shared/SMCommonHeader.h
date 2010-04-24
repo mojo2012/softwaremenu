033d630ebc401c6fc521e94a2ad8fa8701787300
tomcool420 <tomcool420@5333c37c-0b2b-11de-b8f3-b9fb9eb71b04>
2010-04-24 21:43:48 +0000

    misc updates

---
+++ trunk/Shared/SMCommonHeader.h
@@ -0,0 +1,9 @@
+/*
+ *  SMCommonHeader.h
+ *  
+ *
+ *  Created by Thomas Cool on 4/24/10.
+ *  Copyright 2010 Thomas Cool. All rights reserved.
+ *
+ */
+
--- trunk/SoftwareMenuFramework/SMFInvocationCenteredMenuController.m
+++ trunk/SoftwareMenuFramework/SMFInvocationCenteredMenuController.m
@@ -31,4 +31,36 @@
         [(NSInvocation *)[_options objectAtIndex:arg1]invoke];
     [[self stack]popController];
 }
++(id)invocationsForObject:(id)target withSelectorVal:(NSString *)selectorString withArguments:(NSArray *)arguments
+{
+    
+    if (selectorString==nil) {
+        return @"none";
+    }
+    SEL theSelector;
+    NSMethodSignature *aSignature;
+    NSInvocation *anInvocation;
+    id b = target;
+    theSelector = NSSelectorFromString(selectorString);
+    //theSelector= @selector(removeFromQueue:);
+    // NSLog(@"selector)
+    aSignature = [[target class] instanceMethodSignatureForSelector:theSelector];
+    //NSLog(@"signature: %@",aSignature);
+    anInvocation = [NSInvocation invocationWithMethodSignature:aSignature];
+    [anInvocation setSelector:theSelector];
+    [anInvocation setTarget:b];
+    if (arguments !=nil)
+    {
+        int i,count=[arguments count];
+        id c;
+        for(i=2;i<count+2;i++)
+        {
+            c=[arguments objectAtIndex:i-2];
+            [anInvocation setArgument:&c atIndex:i];
+        }
+        
+    }
+    [anInvocation retainArguments];
+    return anInvocation;
+}
 @end
