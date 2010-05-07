3f9c8465be62ab21984b07cb53503b90da3487b4
tomcool420 <tomcool420@5333c37c-0b2b-11de-b8f3-b9fb9eb71b04>
2010-05-07 21:02:33 +0000

    non imported code

---
+++ SoftwareMenu/miscCode.h
@@ -0,0 +1,16 @@
+//
+//  miscCode.h
+//  SoftwareMenu
+//
+//  Created by Thomas Cool on 5/6/10.
+//  Copyright 2010 Thomas Cool. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface miscCode : NSObject {
+
+}
+
+@end
